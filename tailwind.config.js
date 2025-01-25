8ee537620ef937dfd463526155dc926a99e9739f
picosphere0 <parsabatani0@gmail.com>
2025-01-25 18:46:54 +0330

    update 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js,css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#f1f0f4',
+        'background': '#070608',
+        'primary': '#afadc1',
+        'secondary': '#614a58',
+        'accent': '#937173',
+       },
+       
+    },
   },
   plugins: [],
 }
